306c202da7384167dd0951120ae3325b93c03ece
Michael (at Home) <dev@h9h.de>
2019-10-05 17:51:21 +0200

    Porter-Stemmer refactored

--- src/stemmer/german-porter-stemmer.js
+++ src/stemmer/german-porter-stemmer.js
@@ -1,24 +1,18 @@
 /*
   See http://snowball.tartarus.org/algorithms/german/stemmer.html
  */
-module.exports = function (word) {
-	word = word.replace(/([aeiouyäöü])u([aeiouyäöü])/g, '$1U$2')
-	word = word.replace(/([aeiouyäöü])y([aeiouyäöü])/g, '$1Y$2')
-	word = word.replace(/ß/g, 'ss')
-	word = word.replace(/ae/g, 'ä')
-	word = word.replace(/oe/g, 'ö')
-	word = word.replace(/([^q])ue/g, '$1ü')
 
-	/*
-  R1 and R2 are first set up in the standard way:
-    R1 is the region after the first non-vowel following a vowel, or is the null
-    region at the end of the word if there is no such non-vowel.
-    R2 is the region after the first non-vowel following a vowel in R1, or is
-    the null region at the end of the word if there is no such non-vowel.
+const replaceUmlaute = word => {
+	return word
+		.replace(/([aeiouyäöü])u([aeiouyäöü])/g, '$1U$2')
+		.replace(/([aeiouyäöü])y([aeiouyäöü])/g, '$1Y$2')
+		.replace(/ß/g, 'ss')
+		.replace(/ae/g, 'ä')
+		.replace(/oe/g, 'ö')
+		.replace(/([^q])ue/g, '$1ü')
+}
 
-  But then R1 is adjusted so that the region before it contains at least 3 letters.
-  */
-
+function findR1AndR2(word) {
 	let r1Index = word.search(/[aeiouyäöü][^aeiouyäöü]/)
 	let r1 = ''
 	if (r1Index > -1) {
@@ -39,6 +33,79 @@ module.exports = function (word) {
 		r1Index = 3
 	}
 
+	return {r1Index, r2Index}
+}
+
+const removeEnding = (word, ending, r1r2Index, addand = 0) => {
+	let optionIndex = word.search(ending)
+	if (optionIndex > -1) {
+		optionIndex += addand
+		if (optionIndex >= r1r2Index) {
+			return word.substring(0, optionIndex)
+		}
+	}
+
+	return word
+}
+
+const findStep1Endings = word => {
+	const cPos = word.search(/([bdfghklmnrt]s)$/g)
+	return [
+		word.search(/(em|ern|er)$/g),
+		word.search(/(e|en|es)$/g),
+		cPos > -1 ? cPos + 1 : cPos
+	]
+}
+
+const findStep2Endings = word => {
+	const bPos = word.search(/(.{3}[bdfghklmnt]st)$/g)
+	return [
+		word.search(/(en|er|est)$/g),
+		bPos > -1 ? bPos + 4 : bPos
+	]
+}
+
+const findStep3Endings = word => {
+	const bPos = word.search(/[^e](ig|ik|isch)$/g)
+	return [
+		word.search(/(end|ung)$/g),
+		bPos > -1 ? bPos + 1 : bPos,
+		word.search(/(lich|heit)$/g),
+		word.search(/(keit)$/g)
+	]
+}
+
+const checkNonNegButLessThan = (index, index3) => index > -1 && index < index3
+
+const searchLongestSuffix = indices => {
+	let foundIndex = 10000
+	let option = ''
+
+	indices.forEach((index, i) => {
+		if (checkNonNegButLessThan(index, foundIndex)) {
+			option = String.fromCharCode(97 + i) // 97 == 'a'
+			foundIndex = index
+		}
+	})
+
+	return {option, index: foundIndex}
+}
+
+module.exports = function (givenWord) {
+	let word = replaceUmlaute(givenWord)
+
+	/*
+  R1 and R2 are first set up in the standard way:
+    R1 is the region after the first non-vowel following a vowel, or is the null
+    region at the end of the word if there is no such non-vowel.
+    R2 is the region after the first non-vowel following a vowel in R1, or is
+    the null region at the end of the word if there is no such non-vowel.
+
+  But then R1 is adjusted so that the region before it contains at least 3 letters.
+  */
+
+	const {r1Index, r2Index} = findR1AndR2(word)
+
 	/*
   Define a valid s-ending as one of b, d, f, g, h, k, l, m, n, r or t.
   Define a valid st-ending as the same list, excluding letter r.
@@ -55,29 +122,8 @@ module.exports = function (word) {
   (b) e en es
   (c) s (preceded by a valid s-ending)
   */
-	const a1Index = word.search(/(em|ern|er)$/g)
-	const b1Index = word.search(/(e|en|es)$/g)
-	let c1Index = word.search(/([bdfghklmnrt]s)$/g)
-	if (c1Index > -1) {
-		c1Index++
-	}
-
-	let index1 = 10000
-	let optionUsed1 = ''
-	if (a1Index > -1 && a1Index < index1) {
-		optionUsed1 = 'a'
-		index1 = a1Index
-	}
-
-	if (b1Index > -1 && b1Index < index1) {
-		optionUsed1 = 'b'
-		index1 = b1Index
-	}
-
-	if (c1Index > -1 && c1Index < index1) {
-		optionUsed1 = 'c'
-		index1 = c1Index
-	}
+	const indices1 = findStep1Endings(word)
+	const {option: optionUsed1, index: index1} = searchLongestSuffix(indices1)
 
 	/*
   And delete if in R1. (Of course the letter of the valid s-ending is
@@ -105,20 +151,8 @@ module.exports = function (word) {
   letters)
   */
 
-	const a2Index = word.search(/(en|er|est)$/g)
-	let b2Index = word.search(/(.{3}[bdfghklmnt]st)$/g)
-	if (b2Index > -1) {
-		b2Index += 4
-	}
-
-	let index2 = 10000
-	if (a2Index > -1 && a2Index < index2) {
-		index2 = a2Index
-	}
-
-	if (b2Index > -1 && b2Index < index2) {
-		index2 = b2Index
-	}
+	const indices2 = findStep2Endings(word)
+	const {index: index2} = searchLongestSuffix(indices2)
 
 	/*
   And delete if in R1.
@@ -149,63 +183,17 @@ module.exports = function (word) {
   if preceded by lich or ig, delete if in R2
   */
 
-	const a3Index = word.search(/(end|ung)$/g)
-	let b3Index = word.search(/[^e](ig|ik|isch)$/g)
-	const c3Index = word.search(/(lich|heit)$/g)
-	const d3Index = word.search(/(keit)$/g)
-	if (b3Index > -1) {
-		b3Index++
-	}
+	const indices3 = findStep3Endings(word)
+	const {option: optionUsed3, index: index3} = searchLongestSuffix(indices3)
 
-	let index3 = 10000
-	let optionUsed3 = ''
-	if (a3Index > -1 && a3Index < index3) {
-		optionUsed3 = 'a'
-		index3 = a3Index
-	}
-
-	if (b3Index > -1 && b3Index < index3) {
-		optionUsed3 = 'b'
-		index3 = b3Index
-	}
-
-	if (c3Index > -1 && c3Index < index3) {
-		optionUsed3 = 'c'
-		index3 = c3Index
-	}
-
-	if (d3Index > -1 && d3Index < index3) {
-		optionUsed3 = 'd'
-		index3 = d3Index
-	}
-
-	if (index3 !== 10000 && r2Index > -1) {
-		if (index3 >= r2Index) {
-			word = word.substring(0, index3)
-			let optionIndex = -1
-			if (optionUsed3 === 'a') {
-				optionIndex = word.search(/[^e](ig)$/)
-				if (optionIndex > -1) {
-					optionIndex++
-					if (optionIndex >= r2Index) {
-						word = word.substring(0, optionIndex)
-					}
-				}
-			} else if (optionUsed3 === 'c') {
-				optionIndex = word.search(/(er|en)$/)
-				if (optionIndex > -1) {
-					if (optionIndex >= r1Index) {
-						word = word.substring(0, optionIndex)
-					}
-				}
-			} else if (optionUsed3 === 'd') {
-				optionIndex = word.search(/(lich|ig)$/)
-				if (optionIndex > -1) {
-					if (optionIndex >= r2Index) {
-						word = word.substring(0, optionIndex)
-					}
-				}
-			}
+	if (index3 !== 10000 && checkNonNegButLessThan(r2Index, index3 + 1)) {
+		word = word.substring(0, index3)
+		if (optionUsed3 === 'a') {
+			word = removeEnding(word, /[^e](ig)$/, r2Index, 1)
+		} else if (optionUsed3 === 'c') {
+			word = removeEnding(word, /(er|en)$/, r1Index)
+		} else if (optionUsed3 === 'd') {
+			word = removeEnding(word, /(lich|ig)$/, r2Index)
 		}
 	}
 
@@ -214,11 +202,12 @@ module.exports = function (word) {
   turn U and Y back into lower case, and remove the umlaut accent from
   a, o and u.
   */
-	word = word.replace(/U/g, 'u')
-	word = word.replace(/Y/g, 'y')
-	word = word.replace(/ä/g, 'a')
-	word = word.replace(/ö/g, 'o')
-	word = word.replace(/ü/g, 'u')
+	word = word
+		.replace(/U/g, 'u')
+		.replace(/Y/g, 'y')
+		.replace(/ä/g, 'a')
+		.replace(/ö/g, 'o')
+		.replace(/ü/g, 'u')
 
 	return word
 }
